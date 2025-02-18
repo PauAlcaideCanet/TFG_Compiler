2ef61e322325b1b6e929ed388c09aefe727562f2
PauAlcaideCanet <pau.alcaide01@estudiant.upf.edu>
2025-02-18 12:39:00 +0100

    asd

--- src/parser.c
+++ src/parser.c
@@ -1,6 +1,46 @@
-#import <stdlib.h>
+#include <stdio.h>
+#include <stdlib.h>
+#include "token.h"
+#include "table.h"
+#include "stack.h"
 
-int main(){
-    printf("Albert guapo!");
+
+void parse(Token tokens[], int length) {
+    
+    Stack s;
+    init_stack(&s);
+    
+    push(&s, 0);  // Initial state
+    int i = 0;    // Counter of the tokens
+    
+    while (1) {     //While true
+        int state = peek(&s);
+        int symbol = tokens[i].type; // Use TokenType directly as index
+
+        Action action = action_table[state][symbol];
+
+        if (action.type == SHIFT) {
+            push(&s, action.state);
+            i++; //Go read the next token
+        } else if (action.type == REDUCE) {
+            Production_rule p = rules[action.state];
+            for (int j = 0; j < p.rhs_size; j++){ 
+                pop(&s); // Pop RHS symbols
+            } 
+            int new_state = goto_table[peek(&s)][p.lhs - 2]; // Goto based on LHS
+            push(&s, new_state);
+        } else if (action.type == ACCEPT) {
+            printf("Parsing successful!\n");
+            return;
+        } else {
+            printf("Syntax error!\n");
+            return;
+        }
+    }
+}
+
+
+int main() {
+    
     return 0;
 }
