512ff88bff74f8520744bf24ee1138b1f260c403
PauAlcaideCanet <pau.alcaide01@estudiant.upf.edu>
2025-02-18 12:38:28 +0100

    Implementation of the stack

---
+++ src/stack.c
@@ -0,0 +1,34 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include "stack.h"
+
+
+void init_stack(Stack* stack) {
+    stack->top = -1;
+}
+
+
+void push(Stack* stack, int state) {
+    if (stack->top < MAX_STACK - 1) {
+        stack->top++;
+        stack->items[stack->top] = state;
+    } else {
+        printf("Error: Stack overflow!\n");
+    }
+}
+
+int pop(Stack* stack) {
+    if (stack->top >= 0) {
+        stack->top--;
+        return stack->items[stack->top];
+    }
+    printf("Error: Stack underflow!\n");
+}
+
+// Peek function
+int peek(Stack* stack) {
+    if (stack->top >= 0) {
+        return stack->items[stack->top];
+    }
+    printf("Error: Stack is empty!\n");
+}
---
+++ src/stack.h
@@ -0,0 +1,17 @@
+#ifndef STACK_H
+#define STACK_H
+
+#define MAX_STACK 100
+
+// Stack structure
+typedef struct {
+    int items[MAX_STACK];
+    int top;
+} Stack;
+
+void init_stack(Stack* stack);
+void push(Stack* stack, int state);
+int pop(Stack* stack);
+int peek(Stack* stack);
+
+#endif // STACK_H
