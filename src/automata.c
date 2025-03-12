0cc407ed55a414bbc6e87939787d20051b4a653f
PauAlcaideCanet <pau.alcaide01@estudiant.upf.edu>
2025-03-12 19:04:12 +0100

    Shift and Reduce
    Implemented functions Shift and Reduce
    and added some comments

--- src/automata.c
+++ src/automata.c
@@ -4,11 +4,10 @@
 #include "automata.h"
 
 // Get the Rules from its definition
-Production_rule* getProductionRules() {     //Funciona!
-
+Production_rule* getProductionRules() {   
+    // Initialize a temporal variable with the saw info  
     static const char* raw_prod_rules[NUM_RULES][3][MAX_RHSS][2] = PROD_RULES;
-
-
+    // Allocate all memory for the production rules
     Production_rule* prod_list = malloc(NUM_RULES * sizeof(Production_rule));
     if (!prod_list) {
         printf("Memory allocation failed in production rules");
@@ -16,31 +15,29 @@ Production_rule* getProductionRules() {     //Funciona!
     }
 
     for (int i = 0; i < NUM_RULES; i++) {
-
+        // Fill the Left thand side with its token
         prod_list[i].lhs = createToken(getTokenCategory(raw_prod_rules[i][0][0][0]), raw_prod_rules[i][0][0][1]);
-
+        
         prod_list[i].rhs_size = atoi(raw_prod_rules[i][2][0][0]);  
-
+        // Allocate memory for the Right Hand Side
         prod_list[i].rhs = malloc(prod_list[i].rhs_size * sizeof(Token));
         if (!prod_list[i].rhs) {
             fprintf(stderr, "Memory allocation failed for RHS of rule %d\n", i);
             exit(EXIT_FAILURE);
         }
-
-
+        // Fill the information of the Right Hand Side
         for (int j = 0; j < prod_list[i].rhs_size; j++) { 
             prod_list[i].rhs[j] = createToken(getTokenCategory(raw_prod_rules[i][1][j][0]), raw_prod_rules[i][1][j][1]);
         }
-
-
     }
 
     return prod_list;
 }
 
+
+
 // Initialization of the Context-Free Grammar
-void initCFG(CFG *grammar) {        //Segurament funcioni
-    
+void initCFG(CFG *grammar) {         
     // Initialize terminals
     grammar->num_terminals = NUM_TERMINALS;
 
@@ -62,19 +59,22 @@ void initCFG(CFG *grammar) {        //Segurament funcioni
 
     //Initialize the rules
     Production_rule* rules = getProductionRules();
-    grammar->rules = rules;
-    
+    grammar->rules = rules;   
 }
+
+
 
 // Alphabet initialization
 void initAlphabet(const CFG *grammar, Alphabet_symbol* alphabet) { //Funciona
     int index = 0;
+    // Fill the terminals into the alphabet
     for (int i = 0; i < grammar->num_terminals; i++) {
         alphabet[index].symbol = grammar->terminals[i];
         alphabet[index].column = index;
         alphabet[index].is_terminal = 1;
         index++;
     }
+    // Fill the non-terminals into the alphabet
     for (int i = 0; i < grammar->num_non_terminals; i++) {
         alphabet[index].symbol = grammar->non_terminals[i];
         alphabet[index].column = index;
@@ -82,6 +82,8 @@ void initAlphabet(const CFG *grammar, Alphabet_symbol* alphabet) { //Funciona
         index++;
     }
 }
+
+
 
 // Automata initialization
 void initAutomata(const CFG *grammar, Automata* automata) {     //Aixo està bé
@@ -95,7 +97,7 @@ void initAutomata(const CFG *grammar, Automata* automata) {     //Aixo està bé
 
     //Init the accepting states
     int accept_states[] = ACCEPT_STATES;  
-    int num_accept_states = sizeof(accept_states) / sizeof(accept_states[0]); //Divide by the first element (int)
+    int num_accept_states = sizeof(accept_states) / sizeof(accept_states[0]); //Divide by the first element (int) (it can be changed to sizeof(int))
     automata->accepting_states = malloc(num_accept_states * sizeof(int));
     memcpy(automata->accepting_states, accept_states, num_accept_states * sizeof(int));
 
@@ -110,8 +112,9 @@ void initAutomata(const CFG *grammar, Automata* automata) {     //Aixo està bé
             automata->transition_table[i][j] = transition_data[i][j];
         }
     }
-
 }
+
+
 
 // Shift-Reduce Automata initialization
 void initSRAutomata(SR_Automata* sra) {
@@ -119,34 +122,80 @@ void initSRAutomata(SR_Automata* sra) {
         fprintf(stderr, "Error: SR_Automata pointer is NULL\n");
         exit(EXIT_FAILURE);
     }
-
     // Initialize the context-free grammar (CFG)
     initCFG(&sra->grammar);
-
-    // Initialize the parsing stack
+    // Initialize the stack
     init_stack(&sra->stack);
-    push(&sra->stack, START_STATE); // Start state of the automaton
-
+    push(&sra->stack, START_STATE); 
     // Initialize the automaton using the grammar
     initAutomata(&sra->grammar, &sra->automata);
 }
+
+
 
 // Returns the associated column for the introduced token 
 int getColumn(Token token, Alphabet_symbol *alphabet, int num_symbols) {
     for (int i = 0; i < num_symbols; i++) {
         if(alphabet[i].is_terminal){
+            // If it is terminal, return the category
             if (strcmp(getCategoryFromToken(token), alphabet[i].symbol) == 0) {
                 return alphabet[i].column;
             }
         }else{
+            //If it is non-terminal, returns the lexeme which is the category of non terminal
             if (strcmp(token.lexeme, alphabet[i].symbol) == 0) {
                 return alphabet[i].column;
             }
-        }
-        
+        }    
     }
     return -1; // Error case
 }
+
+
+
+// Implementation of the shift action
+int shift(SR_Automata *sra, Action action){
+    push(&sra->stack, action.state);
+    return SHIFT;
+}
+
+
+
+int reduce(SR_Automata *sra, Action action, Token input_token){
+    // Fetch the rule to reduce 
+    Production_rule rule = sra->grammar.rules[action.state];
+
+    // Pop the RHS symbols from the stack
+    for (int i = 0; i < rule.rhs_size; i++) {
+        pop(&sra->stack);
+    }
+
+    // Get the current state after popping
+    int new_state = peek(&sra->stack);
+    
+    // Find the column index of the LHS in the automaton's alphabet
+    int lhs_column = getColumn(rule.lhs, sra->automata.alphabet, sra->automata.num_symbols);
+    if (lhs_column == -1) {
+        printf("ERROR: Could not find LHS '%s' in the alphabet!\n", rule.lhs.lexeme);
+        exit(EXIT_FAILURE);
+    }
+
+    // Get the transition state for the LHS
+    Action goto_action = sra->automata.transition_table[new_state][lhs_column];
+    if (goto_action.type == ERROR) {
+        printf("ERROR: No transition found for LHS '%s' in state %d!\n", rule.lhs.lexeme, new_state);
+        exit(EXIT_FAILURE);
+    }
+
+    // Push the new state after reduction
+    push(&sra->stack, goto_action.state);
+
+    // Call it recursively in case there are more reduces to do before the next shift
+    int n = SRAutomata_step(sra, input_token); 
+    return n;
+}
+
+
 
 // Executes a step of the Shift-Reduce Automata
 int SRAutomata_step(SR_Automata *sra, Token input_token) {
@@ -156,8 +205,6 @@ int SRAutomata_step(SR_Automata *sra, Token input_token) {
     }
 
     int state = peek(&sra->stack);
-    printf("The current state is: %d\n", state);
-    printf("And the look ahead is: %s\n", input_token.lexeme);
     int column = getColumn(input_token, sra->automata.alphabet, sra->automata.num_symbols);
 
     if (column == -1) {
@@ -169,60 +216,31 @@ int SRAutomata_step(SR_Automata *sra, Token input_token) {
 
     switch (action.type) {
         case SHIFT:
-            push(&sra->stack, action.state);
-            printf("SHIFT: Moved to state %d\n", action.state);
+            return shift(sra, action);
             break;
         
         case REDUCE: {
-            // Fetch the rule to reduce with
-            Production_rule rule = sra->grammar.rules[action.state];
-            printf("REDUCE: Applying rule %d -> %s\n", action.state, rule.lhs.lexeme);
-            printf("The LHS category is: %d\n", rule.lhs.category);
-
-            // Pop the RHS symbols from the stack
-            for (int i = 0; i < rule.rhs_size; i++) {
-                pop(&sra->stack);
-            }
-
-            // Get the current state after popping
-            int new_state = peek(&sra->stack);
-            
-            // Find the column index of the LHS in the automaton's alphabet
-            int lhs_column = getColumn(rule.lhs, sra->automata.alphabet, sra->automata.num_symbols);
-            if (lhs_column == -1) {
-                printf("ERROR: Could not find LHS '%s' in the alphabet!\n", rule.lhs.lexeme);
-                exit(EXIT_FAILURE);
-            }
-
-            // Get the transition state for the LHS
-            Action goto_action = sra->automata.transition_table[new_state][lhs_column];
-            if (goto_action.type == ERROR) {
-                printf("ERROR: No transition found for LHS '%s' in state %d!\n", rule.lhs.lexeme, new_state);
-                exit(EXIT_FAILURE);
-            }
-
-            // Push the new state after reduction
-            push(&sra->stack, goto_action.state);
-            printf("REDUCE: Pushed new state %d after reducing to %s\n", goto_action.state, rule.lhs.lexeme);
-
-            int n = SRAutomata_step(sra, input_token); 
-            return n;
+            return reduce(sra, action, input_token);
+            break;
         }
         
         case ACCEPT:
             printf("Input accepted.\n");
+            return ACCEPT;
             break;
         
         case ERROR:
             printf("There has been an error in the parsing process!\nAt token '%s'", input_token.lexeme);
-            return -1;
+            return ERROR;
         default:
             printf("Syntax error at token '%s'.\n", input_token.lexeme);
     }
-    return 0;
+    return ERROR;
 }
 
-// Free the memory of the CFG ---------REVISAR-----------
+
+
+// Free the memory of the CFG 
 void freeCFG(CFG *grammar) {
     if (!grammar) return;
 
@@ -253,7 +271,8 @@ void freeCFG(CFG *grammar) {
 }
 
 
-// Free the memory of the Automata   ---------REVISAR-----------
+
+// Free the memory of the Automata   
 void freeAutomata(Automata *automata) {
     if (!automata) return;
 
@@ -281,7 +300,9 @@ void freeAutomata(Automata *automata) {
     }
 }
 
-// Free the memory of the Shift-Reduce Automata --------REVISAR--------
+
+
+// Free the memory of the Shift-Reduce Automata 
 void freeSR_Automata(SR_Automata *sra) {
     if (!sra) return;
 
--- src/main.c
+++ src/main.c
@@ -24,7 +24,7 @@ int main() {
     // Process each token using the Shift-Reduce Automaton
     int step = 0;
     int i = 0;
-    while (i < num_tokens && step != -1) {
+    while (i < num_tokens && step != ERROR) {
         step = SRAutomata_step(&sra, tokens[i]);
         freeToken(&tokens[i]);
 
