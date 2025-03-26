d0eb3f827588f3769b1d3f6172e265f7c2a23d57
PauAlcaideCanet <pau.alcaide01@estudiant.upf.edu>
2025-03-26 14:21:06 +0100

    Node created

---
+++ src/node.c
@@ -0,0 +1,84 @@
+#include "node.h"
+
+// Create an Integer Node
+IntNode *createIntegerNode(int value) {
+    IntNode *node = (IntNode *)malloc(sizeof(IntNode));
+    node->base.type = NODE_INTEGER;
+    node->value = value;
+    return node;
+}
+
+
+// Create a Binary Operation Node
+BinaryOperationNode *createBinaryOperationNode(char operation, Expression *lhs, Expression *rhs) {
+    BinaryOperationNode *node = (BinaryOperationNode *)malloc(sizeof(BinaryOperationNode));
+    node->base.type = NODE_BINARY_OPERATION;
+    node->operation = operation;
+    node->lhs = lhs;
+    node->rhs = rhs;
+    return node;
+}
+
+
+// Create a Grouping Node
+GroupingNode *createGroupingNode(Expression *expression) {
+    GroupingNode *node = (GroupingNode *)malloc(sizeof(GroupingNode));
+    node->base.type = NODE_GROUPING;
+    node->expression = expression;
+    return node;
+}
+
+
+
+// Print AST (Recursive)
+void printAST(Expression *node) {
+    if (!node) return;
+
+    switch (node->type) {
+        case NODE_INTEGER:
+            printf("%d", ((IntNode *)node)->value);
+            break;
+
+        case NODE_BINARY_OPERATION: {
+            BinaryOperationNode *binOp = (BinaryOperationNode *)node;
+            printf("(");
+            printAST(binOp->lhs);
+            printf(" %c ", binOp->operation);
+            printAST(binOp->rhs);
+            printf(")");
+            break;
+
+        }
+        case NODE_GROUPING: {
+            GroupingNode *group = (GroupingNode *)node;
+            printf("(");
+            printAST(group->expression);
+            printf(")");
+            break;
+
+        }
+    }
+}
+
+
+
+void freeAST(Expression *node) {
+    if (!node) return;
+
+    switch (node->type) {
+        case NODE_BINARY_OPERATION:
+            freeAST(((BinaryOperationNode *)node)->lhs);
+            freeAST(((BinaryOperationNode *)node)->rhs);
+            break;
+
+        case NODE_GROUPING:
+            freeAST(((GroupingNode *)node)->expression);
+            break;
+
+        case NODE_INTEGER:
+            break;
+
+    }
+
+    free(node);
+}
---
+++ src/node.h
@@ -0,0 +1,49 @@
+/*
+    Base és el que els uneix com a expressions, és el NodeType
+
+*/
+
+#ifndef NODE_H
+#define NODE_H
+
+#include <stdio.h>
+#include <stdlib.h>
+
+// Enumeration of the Type of nodes in the Abstract syntax tree
+typedef enum {
+    NODE_INTEGER,
+    NODE_BINARY_OPERATION,
+    NODE_GROUPING
+} NodeType;
+
+// Base expression structure
+typedef struct Expression {
+    NodeType type;
+} Expression;
+
+// Integer Node
+typedef struct {
+    Expression base;
+    int value;
+} IntNode;
+
+// Binary Operation Node (sum, mult, ...)
+typedef struct {
+    Expression base;
+    char operation;     // '+' or '*'
+    Expression *lhs;
+    Expression *rhs;
+} BinaryOperationNode;
+
+// Grouping Node (for parenthesis)
+typedef struct {
+    Expression base;
+    Expression *expression;
+} GroupingNode;
+
+// AST Root Structure
+typedef struct {
+    Expression *root;  // Root of the AST
+} AST;
+
+#endif // NODE_H
